e969ce74eaf38b800819e67da4ee629f3a621591
Kenneth Black and Luke Philips <kenneth@>
2016-11-28 15:30:10 -0800

    pass unit testing for has_key?

--- lib/hashy.rb
+++ lib/hashy.rb
@@ -9,13 +9,20 @@ class Hashy
     @keys = ["a"]
     @values = [1]
   end
-  define_method(:fetch) do |key|
-    index = @keys.index(key)
-    @values[index]
-  end
   define_method(:store) do |key, value|
     @keys.push(key)
     @values.push(value)
     value
+  end
+  define_method(:fetch) do |key|
+    index = @keys.index(key)
+    @values[index]
+  end
+  define_method(:key) do |value|
+    index = @values.index(value)
+    @keys[index]
+  end
+  define_method(:has_key?) do |key|
+    @keys.include?(key)
   end
 end
--- spec/hashy_spec.rb
+++ spec/hashy_spec.rb
@@ -3,6 +3,12 @@ require("rspec")
 require('hashy')
 
 describe(Hashy) do
+  describe('#store') do
+    it('pushes key and value to corresponding arrays with same index point in each') do
+      test_hashy = Hashy.new()
+      expect(test_hashy.store("a", 1)).to eq 1
+    end
+  end
   describe('#fetch') do
     it('pulls value corresponding to a given key from the values array') do
       test_hashy = Hashy.new()
@@ -10,17 +16,18 @@ describe(Hashy) do
       expect(test_hashy.fetch("a")).to eq(1)
     end
   end
-  describe('#store') do
-    it('pushes key and value to corresponding arrays with same index point in each') do
-      test_hashy = Hashy.new()
-      expect(test_hashy.store("a", 1)).to eq 1
-    end
-  end
   describe('#key') do
     it('returns key of a given value') do
       test_hashy = Hashy.new()
       test_hashy.store("a", 1)
       expect(test_hashy.key(1)).to eq "a"
+    end
+  end
+  describe('#has_key?') do
+    it('returns boolean of whether key exists or not') do
+      test_hashy = Hashy.new()
+      test_hashy.store("a", 1)
+      expect(test_hashy.has_key?("a")).to eq true
     end
   end
 end
